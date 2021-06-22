486e05ce30ac1b3c644d82df24b74c268423cd6b
Minhui Xie <47163571+Pachimarii@users.noreply.github.com>
2021-06-22 16:28:17 -0400

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
+// Coverage Screen
+export { default as SearchBar } from './CoverageScreen/SearchBar';
+export { default as CoverageFilter } from './CoverageScreen/CoverageFilter';
+export { default as CoverageContainer} from './CoverageScreen/CoverageContainer';
+export { default as PapersSection} from './CoverageScreen/PapersSection';
+export { default as WaffleChart} from './CoverageScreen/WaffleChart';
